1190729b988912889884f4d8ee61456b229eb7c3
Antony Abee Prakash XV <antonyabee.prakashxv@adlinktech.com>
2021-11-25 12:12:31 +0530

    Add VR Workaround and downgrade SCP/ATF FW to 1.07
    This patch will add VR workaround, downgrade SCP/ATF FW from 1.08 to 1.07
    and change board name as "COM-HPC-ALT A1" to display in Platform
    Board information setup page.
    
    Signed-off-by: Antony Abee Prakash XV <antonyabee.prakashxv@adlinktech.com>

--- make_ComHpcAlt.sh
+++ make_ComHpcAlt.sh
@@ -10,14 +10,14 @@ make -f $WORKSPACE/edk2-ampere-tools/Makefile \
     VM_SHARED_DIR=$HOME/AmpereR \
     CHECKSUM_TOOL=$OEM_SRC_DIR/toolchain/checksum \
     PACKAGES_PATH=$OEM_SRC_DIR:$WORKSPACE/OpenPlatformPkg:"${PACKAGES_PATH}" \
-    ATF_SLIM=$WORKSPACE/srp-ac01-fw-aptv-bin-r1.08.20210830/altra_firmware_sdk/bin/atf/altra_atf_signed_1.08.20210825.slim \
-    SCP_SLIM=$WORKSPACE/srp-ac01-fw-aptv-bin-r1.08.20210830/altra_firmware_sdk/bin/scp/altra_scp_signed_1.08.20210825.slim \
+    ATF_SLIM=$WORKSPACE/srp-ac01-fw-aptv-bin-r1.07a.20210713/altra_firmware_sdk/bin/atf/altra_atf_signed_1.07.20210713.slim \
+    SCP_SLIM=$WORKSPACE/srp-ac01-fw-aptv-bin-r1.07a.20210713/altra_firmware_sdk/bin/scp/altra_scp_signed_1.07.20210713.slim \
     LINUXBOOT_BIN=$WORKSPACE/flashkernel \
     SPI_SIZE_MB=32 \
-    VER=1.07 BUILD=300 \
-    tianocore_img # tianocore_capsule # linuxboot_img # all # 
+    VER=1.07 BUILD=300.02 \
+    tianocore_img # tianocore_capsule # linuxboot_img # all #
 # below setting is for module not reworked fo VR circuit, insert them to above making arguments 
 # to support the modules without reworking.    
-#    FAILSAFE_WORKAROUND=1 \
-#    BOARD_SETTING=$OEM_SRC_DIR/Platform/Ampere/ComHpcAltPkg/ComHpcAltBoardSettingVRWA.cfg \
+    FAILSAFE_WORKAROUND=1 \
+    BOARD_SETTING=$OEM_SRC_DIR/Platform/Ampere/ComHpcAltPkg/ComHpcAltBoardSettingVRWA.cfg \
 export PATH=$OPATH
---
+++ srp-ac01-fw-aptv-bin-r1.07a.20210713/altra_firmware_sdk/bin/atf/Readme.txt
@@ -0,0 +1,3 @@
+# ATF
+
+The ATF binaries are necessary for UEFI (EDK2, APTIOV or similar) projects. For AMI-based developments or deployments please find the integrated binary in your AptioV SRP. For EDK2 and other non-AMI projects, use the included atf binary with the documented build process.
