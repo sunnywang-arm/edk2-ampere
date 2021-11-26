d2b8a6537ecbc40b4c840917ea334e1c5c77e03e
Antony Abee Prakash XV <antonyabee.prakashxv@adlinktech.com>
2021-11-26 14:55:29 +0530

    Make all types of images during build
    Signed-off-by: Antony Abee Prakash XV <antonyabee.prakashxv@adlinktech.com>

--- make_ComHpcAlt.sh
+++ make_ComHpcAlt.sh
@@ -15,7 +15,7 @@ make -f $WORKSPACE/edk2-ampere-tools/Makefile \
     LINUXBOOT_BIN=$WORKSPACE/flashkernel \
     SPI_SIZE_MB=32 \
     VER=1.07 BUILD=300.02 \
-    tianocore_img # tianocore_capsule # linuxboot_img # all #
+    all # tianocore_img # tianocore_capsule # linuxboot_img #
 # below setting is for module not reworked fo VR circuit, insert them to above making arguments 
 # to support the modules without reworking.    
     FAILSAFE_WORKAROUND=1 \
